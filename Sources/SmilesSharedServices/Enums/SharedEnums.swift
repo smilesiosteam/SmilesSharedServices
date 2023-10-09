095235acb12baa7dfdbd87c3b5a79b0e15d1ea00
Habib Rehman <haburrehman@eand.com>
2023-10-09 12:38:41 +0500

    -Created Sharedenums

---
+++ Sources/SmilesSharedServices/Enums/SharedEnums.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Habib Rehman on 09/10/2023.
+//
+
+import Foundation
+
+public enum RestaurantMenuType: String, Codable {
+    case DELIVERY, PICK_UP
+}
